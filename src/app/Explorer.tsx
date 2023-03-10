e44ce55555c397b669bd879d34ef57c0d4cd91e5
Jaeho Lee <jaeho@dev.thingd.com>
2023-03-10 14:51:16 +0900

    refactor: remove `prefetch` behavior from link, interface change

--- src/app/Explorer.tsx
+++ src/app/Explorer.tsx
@@ -27,7 +27,7 @@ export default async function Explorer({ userPath }: { userPath: string }) {
         <Dir key={`dir-${ent.name}`} userPath={userPath} dirPath={ent.name} />
       ))}
       {files.map((ent) => (
-        <File key={ent.name}>{ent.name}</File>
+        <File key={ent.name} name={ent.name} />
       ))}
     </div>
   )
@@ -37,20 +37,14 @@ function Entry({ children }: { children: React.ReactNode }) {
   return <div className="p-3 text-xs border-b-[1px] hover:bg-gray-300 cursor-pointer"> {children}</div>
 }
 
-function File({ children }: { children: React.ReactNode }) {
+function File({ name }: { name: string }) {
   // TODO: click to show file info
-  return <Entry>{children}</Entry>
+  return <Entry>{name}</Entry>
 }
 
 function Dir({ userPath, dirPath }: { userPath: string; dirPath: string }) {
-  let pathValue
-  if (dirPath === "..") {
-    pathValue = `/?path=${path.resolve(dirPath, userPath)}`
-  } else {
-    pathValue = `/?path=${path.resolve(userPath, dirPath)}`
-  }
   return (
-    <Link href={`/?path=${path.resolve(userPath, dirPath)}`}>
+    <Link href={`/?path=${path.resolve(userPath, dirPath)}`} prefetch={false}>
       <Entry>
         {`> `} {dirPath}
       </Entry>
